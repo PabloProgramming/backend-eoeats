d706d74acf35f0903339f7cb474dc184b0a571a1
Pablo <pmontalvoster@gmail.com>
2025-04-26 13:26:44 +0100

    Safe app.properties

--- src/main/java/com/app/eoeats/EoeatsApplication.java
+++ src/main/java/com/app/eoeats/EoeatsApplication.java
@@ -2,12 +2,9 @@ package com.app.eoeats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.app.eoeats.repository")
-@EntityScan("com.app.eoeats.model")
+
 public class EoeatsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EoeatsApplication.class, args);
