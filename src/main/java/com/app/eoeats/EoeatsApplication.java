6eeae412060a8729d51dd49788ca54a816b0db46
Pablo <pmontalvoster@gmail.com>
2025-04-26 11:39:43 +0100

    add port random picker

--- src/main/java/com/app/eoeats/EoeatsApplication.java
+++ src/main/java/com/app/eoeats/EoeatsApplication.java
@@ -2,14 +2,22 @@ package com.app.eoeats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.app.eoeats.repository")
-@EntityScan("com.app.eoeats.model")
 public class EoeatsApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(EoeatsApplication.class, args);
+		SpringApplication app = new SpringApplication(EoeatsApplication.class);
+
+		// Manually set the port if PORT env variable is present
+		Map<String, Object> props = new HashMap<>();
+		props.put("server.port", System.getenv("PORT") != null ? System.getenv("PORT") : "8080");
+		app.setDefaultProperties(props);
+
+		app.run(args);
 	}
 }
+
